cdc77f707fa521781d629eec94dc607de9db5303
Irfan <irfan.140895@gmail.com>
2023-05-22 10:44:38 +0800

    fix(api): change complaint request body

--- Complaint API/Controllers/ComplaintController.cs
+++ Complaint API/Controllers/ComplaintController.cs
@@ -1,17 +1,59 @@
 ﻿using Complaint_API.Base;
 using Complaint_API.Models;
 using Complaint_API.Repository.Contracts;
+using Complaint_API.ViewModels;
+using Complaint_API.ViewModels.Request;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Complaint_API.Controllers
 {
     [Authorize]
     public class ComplaintController : BaseController<IComplaintRepository, Complaint, int>
     {
-        public ComplaintController(IComplaintRepository repository) : base(repository)
+        public ComplaintController(IComplaintRepository repository) : base(repository) { }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [NonAction]
+        public override async Task<ActionResult> InsertAsync(Complaint entity)
         {
-            
+            var result = await _repository.InsertAsync(entity);
+            if (result is null)
+            {
+                return Conflict(new
+                {
+                    statusCode = HttpStatusCode.Conflict,
+                    message = "Data Fail to Insert!"
+                });
+            }
+            return new ObjectResult(new ResultFormat
+            {
+                StatusCode = 201,
+                Status = "Success",
+                Message = "Data Saved Successfully!",
+                Data = result
+            })
+            {
+                StatusCode = 201
+            };
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> InserComplainttAsync(ComplaintVM request)
+        {
+            Complaint entity = new Complaint
+            {
+                Title = request.Title,
+                Description = request.Description,
+                OrderId = request.OrderId,
+                Status = 0,
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now,
+            };
+
+            var result = await InsertAsync(entity);
+            return result;
         }
     }
 }
---
+++ Complaint API/ViewModels/Request/ComplaintVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Complaint_API.ViewModels.Request
+{
+    public class ComplaintVM
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public int OrderId { get; set; }
+    }
+}
