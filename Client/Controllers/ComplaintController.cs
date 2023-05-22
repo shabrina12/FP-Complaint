bab447d59582a5cf97502c3866372f7159423d14
shabrina12 <shabbrina5@gmail.com>
2023-05-22 13:19:28 +0700

    add PostComplaint request

--- Client/Controllers/ComplaintController.cs
+++ Client/Controllers/ComplaintController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Client.Repository.Interface;
 using Client.Repository;
-using Client.Models;
+using Client.ViewModels;
 
 namespace Client.Controllers
 {
@@ -14,8 +14,34 @@ namespace Client.Controllers
 			this.repository = repository;
 		}
 
-		public IActionResult Index()
+		[HttpGet]
+		public IActionResult AddComplaint()
 		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> AddComplaint(ComplaintVM complaint)
+		{
+			string token = HttpContext.Session.GetString("JWToken");
+			var result = await repository.PostComplaint(complaint, token);
+			
+			if (result is null)
+			{
+				return RedirectToAction("Error", "Home");
+			}
+			else if (result.StatusCode == "409")
+			{
+				ModelState.AddModelError(string.Empty, result.Message);
+				TempData["Error"] = $"Something Went Wrong! - {result.Message}!";
+				return View();
+			}
+			else if (result.StatusCode == "200")
+			{
+				TempData["Success"] = $"Data has been Successfully Registered! - {result.Message}!";
+				return RedirectToAction("Index", "Home");
+			}
 			return View();
 		}
 	}
--- Client/Repository/Interface/IComplaintRepository.cs
+++ Client/Repository/Interface/IComplaintRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Client.Models;
+using Client.ViewModels;
 
 namespace Client.Repository.Interface
 {
     public interface IComplaintRepository : IGeneralRepository<Complaint, int>
     {
-    }
+		public Task<ResponseMessageVM> PostComplaint(ComplaintVM entity, string token);
+	}
 }
