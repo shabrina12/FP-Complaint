a81b1d0af2630a82b6e72d02f71d051a9bb582b5
Shabrina Putri <shabbrina5@gmail.com>
2023-05-24 10:31:52 +0700

    buat tampilan tabel users

---
+++ Client/wwwroot/js/user.js
@@ -0,0 +1,40 @@
+﻿
+var token = sessionStorage.getItem("JWToken");
+var headers = {};
+if (token) {
+    headers.Authorization = 'Bearer ' + token;
+}
+
+$(document).ready(function () {
+    let table = new $('#tableUser').DataTable({
+        ajax: {
+            url: "https://localhost:7127/api/user",
+            dataType: "json",
+            headers: headers,
+            dataSrc: "data"
+        },
+        columns: [
+            {
+                data: "",
+                render: (data, type, row, meta) => {
+                    return meta.row + 1;
+                }
+            },
+            { data: "name" },
+            { data: "email" },
+            { data: "gender" },
+            { data: "birthDate" },
+            { data: "phoneNumber" },
+            { data: "role.name" },
+        ],
+        aLengthMenu: [[5, 10, 15, 20, -1], [5, 10, 15, 20, "All"]],
+        iDisplayLength: 10,
+        dom: 'lBfrtip',
+        buttons: [
+            'copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5'
+        ]
+    });
+    setInterval(function () {
+        table.ajax.reload();
+    }, 30000);
+});
